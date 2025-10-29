1eabb7d58aaa6b504f00a3762e52302f4bec9074
malo93777 <simonetagliento@gmail.com>
2025-10-29 16:12:27 +0100

    corretto path src 2

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ import streamlit as st
 import sys
 from pathlib import Path
 
-# Aggiunge la cartella "src" al percorso di ricerca dei moduli
+# Imposta il percorso al modulo src/
 BASE_DIR = Path(__file__).resolve().parent
-SRC_DIR = BASE_DIR
+SRC_DIR = BASE_DIR / "src"
 if str(SRC_DIR) not in sys.path:
     sys.path.append(str(SRC_DIR))
 
-# Percorso al file principale della tua app Streamlit
+# Importa e avvia la pagina principale
 APP_PATH = SRC_DIR / "pages" / "compare_players.py"
 
-# Esegue lo script principale
 with open(APP_PATH, encoding="utf-8") as f:
     code = compile(f.read(), str(APP_PATH), "exec")
     exec(code, globals())
