84617b611305e9648989fd9005224a91ad96b747
malo93777 <simonetagliento@gmail.com>
2026-01-30 10:29:27 +0100

    Update model_predict_voto.py

--- src/model_predict_voto.py
+++ src/model_predict_voto.py
@@ -479,6 +479,7 @@ def predizioni_per_ruolo(df_voti, next_games_df, pipeline=None, top_n=5):
             team = df_voti.loc[df_voti['player_norm'] == player, 'player_team'].iloc[0]
             if team is None or pd.isna(team): #prendendo l'ultima squadra, se il giocatore va all'estero a gennaio non trova più team
                 print(f"Player {player} è andato all'estero, squadra non trovata")
+                team == "squadra sconosciuta"
             team = team.lower().strip()
             teams_role.append(team)
             
