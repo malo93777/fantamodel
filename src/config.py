0499fbeb0a1eeb93bd007798f0bccde9282c2a25
malo93777 <simonetagliento@gmail.com>
2026-02-13 14:23:41 +0100

    fix

--- src/config.py
+++ src/config.py
@@ -290,4 +290,4 @@ manual_roles = {
 
 PREFIXES = {'de','da','di','del','do','van','von','der','le','la','el','al','du','ze'}
 
-GONE_PLAYERS_NOTT_TOP5_LEAGUES = {"noa lang"}
+GONE_PLAYERS_NOT_TOP5_LEAGUES = {"noa lang"}
