2f7aadea3417bb27cc8a17f412bef1cdb11c0da0
malo93777 <simonetagliento@gmail.com>
2026-09-01 16:30:18 +0200

    fix

--- src/config.py
+++ src/config.py
@@ -298,7 +298,7 @@ manual_roles = {
 
 PREFIXES = {'de','da','di','del','do','van','von','der','le','la','el','al','du','ze'}
 
-#GONE_PLAYERS_NOT_TOP5_LEAGUES = {"noa lang", "edin dzeko", "matteo guendouzi","dennis johnsen", "albert gronbaek", "franco vazquez"}
+GONE_PLAYERS_NOT_TOP5_LEAGUES = {}
 
 POSTPONED_TEAMS_2026 = {
    
