8a047b26aee0e77b4bb19dc6c75d091799d0d5bf
malo93777 <simonetagliento@gmail.com>
2026-09-03 17:30:29 +0200

    Update compare_players.py

--- pages/compare_players.py
+++ pages/compare_players.py
@@ -212,17 +212,17 @@ def main():
     # ⚔️ MOSTRA CONFRONTO
     # ======================================================
     #DEBUG
-    compare_btn = True
-    player1 = "Cancellieri"
-    team1 = "Lazio"
-    opponent1 = "Venezia"
-    player2 = "Malen"
-    team2 = "Roma"
-    opponent2 = "Napoli"
-    is_home1 = True
-    is_away1 = False
-    is_home2 = False
-    is_away2 = True
+    #compare_btn = True
+    #player1 = "Cancellieri"
+    #team1 = "Lazio"
+    #opponent1 = "Venezia"
+    #player2 = "Malen"
+    #team2 = "Roma"
+    #opponent2 = "Napoli"
+    #is_home1 = True
+    #is_away1 = False
+    #is_home2 = False
+    #is_away2 = True
     
     if compare_btn:
         if not player1 or not player2:
