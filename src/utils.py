732cd7fc35e376e9dbe56537ddcc8a16b40b667a
malo93777 <simonetagliento@gmail.com>
2026-09-11 15:03:59 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1224,6 +1224,7 @@ def prepare_voto_dataframe(df_raw: pd.DataFrame) -> pd.DataFrame:
     - rimuove senza voto
     - pulisce la posizione
     """
+    
     df = df_raw.copy()
 
     #df = df[df['season'] == config.CURRENT_SEASON] #TOLGO PER ORA NON HO ABBASTANZA DATI DI QUESTA STAGIONE
@@ -1234,9 +1235,6 @@ def prepare_voto_dataframe(df_raw: pd.DataFrame) -> pd.DataFrame:
 
     # pulizia posizione
     df['position_clean'] = df['position'].apply(clean_position)
-
-    #rimuovo tutte le righe che non hanno nella colonna player_team una squadra presente in config.SERIA_TEAMS_CURRENT_SEASON
-    df = df[df['player_team'].isin(config.SERIA_TEAMS_CURRENT_SEASON)]
 
     return df
 
